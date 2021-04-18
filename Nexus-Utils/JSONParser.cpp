e36eea591276674acba77b933ea77309df888e21
Selis <selis@buckicompany.com>
2021-04-18 20:51:54 +0200

    Added first JSON Parsing

--- Nexus-Utils/JSONParser.cpp
+++ Nexus-Utils/JSONParser.cpp
@@ -1,6 +1,64 @@
 #include "pch.h"
 #include "JSONParser.h"
+#include <fstream>
+#pragma warning(disable : 4996)
 
-std::string JSONParser::read(char* file) {
-	return "None";
+bool JSONParser::getBool(char* path, char* key) {
+	std::ifstream file(path);
+	Json::Value json;
+	Json::Reader reader;
+
+	reader.parse(file, json);
+
+	return json[key].asBool();
+}
+
+double JSONParser::getDouble(char* path, char* key) {
+	std::ifstream file(path);
+	Json::Value json;
+	Json::Reader reader;
+
+	reader.parse(file, json);
+
+	return json[key].asDouble();
+}
+
+float JSONParser::getFloat(char* path, char* key) {
+	std::ifstream file(path);
+	Json::Value json;
+	Json::Reader reader;
+
+	reader.parse(file, json);
+
+	return json[key].asFloat();
+}
+
+int JSONParser::getInt(char* path, char* key) {
+	std::ifstream file(path);
+	Json::Value json;
+	Json::Reader reader;
+
+	reader.parse(file, json);
+	
+	return json[key].asInt();
+}
+
+std::string JSONParser::getString(char* path, char* key) {
+	std::ifstream file(path);
+	Json::Value json;
+	Json::Reader reader;
+
+	reader.parse(file, json);
+
+	return json[key].asString();
+}
+
+Json::Value JSONParser::getJson(char* path) {
+	std::ifstream file(path);
+	Json::Value json;
+	Json::Reader reader;
+
+	reader.parse(file, json);
+
+	return json;
 }
--- Nexus-Utils/JSONParser.h
+++ Nexus-Utils/JSONParser.h
@@ -1,9 +1,16 @@
 #pragma once
+#include <json/value.h>
+#include <json/json.h>
 #include <string>
 
 class __declspec(dllexport) JSONParser
 {
 public:
-	std::string read(char* file);
-};
+	bool getBool(char* path, char* key);
+	double getDouble(char* path, char* key);
+	float getFloat(char* path, char* key);
+	int getInt(char* path, char* key);
+	std::string getString(char* path, char* key);
 
+	Json::Value getJson(char* path);
+};
